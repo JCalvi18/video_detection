379a26a266e377a2dbf7d88ef26e91bc9891d3c0
Jorge Calvimontes <calvimontesjorges@gmail.com>
2019-09-11 09:16:21 -0400

    Face verification and coordenate approximation

--- vd_retina.py
+++ vd_retina.py
@@ -26,19 +26,38 @@ parser.add_argument('--ga-model', type=str, default='')
 parser.add_argument('--gpu', type=int, default=-1)
 parser.add_argument('--det', type=int, default=0)
 parser.add_argument('--flip', type=int, default=0)
-parser.add_argument('--threshold', type=float, default=0.9)
-parser.add_argument('--threshold-face', type=float, default=0.4)
-parser.add_argument('--prepare', action='store_true', help='This is a boolean')
-parser.add_argument('--pose', action='store_true', help='Temporary flag to test only face identification')
-
+parser.add_argument('--threshold', type=float, default=0.9, help='Threshold for retinaface, face detection')
+parser.add_argument('--threshold-face', type=float, default=0.4, help='Threshold for mtcnn face verification')
+parser.add_argument('--threshold-l2', type=float, default=200.0, help='Threshold for l2 distance')
+parser.add_argument('--prepare', action='store_true', help='Create the dataset based on the images of --faces_dir')
+parser.add_argument('--poses', type=str, default='1.2,1.2,2.0,3.0', help='LRUD')
 
 def hex2rgb(h):
     return tuple(int(h[i:i + 2], 16) for i in (0, 2, 4))
 
 
-box_colors = ['a50104', '261c15', 'ff01fb', '2e1e0f', '003051', 'f18f01', '6e2594']
+box_colors = ['a50104', '261c15', 'ff01fb', '2e1e0f', '003051', 'f18f01', '6e2594', 'FFFFFF']
 box_colors = [hex2rgb(h) for h in box_colors]
 pose_titles = ['Ret', 'L', 'R', 'U', 'D']
+
+
+class Person(object):
+    def __init__(self, pre_point, box, l2_thresh):
+        self.pre_point = pre_point  # Center point of the face
+        self.box = box  # Bounding box of the face
+        self.name = None
+        self.face = None  # A cropped image of the face
+        self.known = None  # Is in the dataset?
+        self.l2_thresh = l2_thresh
+
+    def l2_distance(self, point):
+        # Compare actual point with previous point
+        # If l2 distance > l2 threshold return False else the value
+        l2 = (((point[0] - self.pre_point[0]) ** 2) + ((point[1] - self.pre_point[1]) ** 2)) ** 0.5
+        if l2 > self.l2_thresh:
+            return -1
+        else:
+            return l2
 
 
 class VideoDetector(object):
@@ -48,8 +67,11 @@ class VideoDetector(object):
         self.model = face_model.FaceModel(args)
         rtpath, epoch = self.args.rt_model.split(',')
         self.detector = RetinaFace(rtpath, int(epoch), self.args.gpu, 'net3')
-        self.names = None  # Names of the persons in the dataset
         self.dataset = None  # Collection of features of known names
+        self.names = {}  # Names of known person
+        self.persons = []  # List of person detected
+        self.crop_resolution = int(self.args.image_size.split(',')[0])  # Resolution to crop person face
+        self.pv = [float(p) for p in self.args.poses.split(',')]
 
     def prepare_faces(self, dataset_name='dataset.pkl'):
         image_names = os.listdir(self.args.faces_dir)
@@ -81,9 +103,10 @@ class VideoDetector(object):
             start = time()
             ret, frame = cap.read()
             if ret:
-                render = self.detect_faces(frame)
-                for _ in range(5):
-                    renders.append(render)
+                total_boxes, points = self.detect_faces(frame)
+                self.identify(frame, total_boxes, points)
+                render = self.draw_names(frame)
+                renders.append(render)
             frame_time = np.append(frame_time, time() - start)
         cap.release()
         return renders, {'w': frame_w, 'h': frame_h}, {'fr_exec': frame_time.mean()}
@@ -93,7 +116,6 @@ class VideoDetector(object):
         with open(dataset_path + '/' + dataset_name, 'rb') as f:  # Load Dataset on numpy format
             np_dataset = pickle.load(f)
         # Create dictionary with person names and their corresponding feature index
-        self.names = {}
         i = 0
         for k, v in np_dataset.items():
             self.names[k] = slice(i, i + v.shape[0])
@@ -101,16 +123,15 @@ class VideoDetector(object):
         # Transform dataset to mx NDarray format
         self.dataset = nd.array(np.concatenate([v for v in np_dataset.values()]), ctx=self.ctx)
 
-    def draw_names(self, frame, names):
-        # names: dict{'name' : bounding_box}
-        colors = box_colors[:len(names)]
-        for name, b, c in zip(names.keys(), names.values(), colors):
-            if name == 'unknown':
-                for x in b:
-                    cv2.rectangle(frame, (int(x[0]), int(x[1])), (int(x[2]), int(x[3])), colors[-1], 2)
+    def draw_names(self, frame):
+        colors = box_colors[:len(self.persons)]
+        for person, c in zip(self.persons, colors):
+            b = person.box
+            if person.name is None:
+                cv2.rectangle(frame, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), colors[-1], 2)
             else:
                 cv2.rectangle(frame, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), c, 2)
-                cv2.putText(frame, name, (int(b[0]), int(b[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3,
+                cv2.putText(frame, person.name, (int(b[0]), int(b[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3,
                             cv2.LINE_AA)
         return frame
 
@@ -125,6 +146,9 @@ class VideoDetector(object):
                     color = (0, 0, 255)
                     if l == 0 or l == 3:
                         color = (0, 255, 0)
+                    if l == 2:
+                        color = (255, 0, 0)
+
                     cv2.circle(frame, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
 
                 poses = self.detector.check_large_pose(landmark5, box[1:])
@@ -136,47 +160,120 @@ class VideoDetector(object):
                     y += 60
         return frame
 
-    def name_faces(self, persons, total_boxes):
-        faces_names = {}
-        unknown_faces = []
-        for person, box in zip(persons, total_boxes):
-            face = self.model.get_input(person)
-            if face is None:
-                continue
-            face = nd.array(self.model.get_feature(face), ctx=self.ctx)
+    def name_face(self, person_face):
+        ## Name the face of a person based on the dataset
+        face = self.model.get_input(person_face)
+        if face is None:
+            return None
+        face = nd.array(self.model.get_feature(face), ctx=self.ctx)
 
-            # Calculate the similarity between the known features and the current face feature
-            sim = nd.dot(self.dataset, face)
-            scores = {}
-            for known_id, index in self.names.items():
-                scores[known_id] = max(sim[index]).asnumpy()
+        # Calculate the similarity between the known features and the current face feature
+        sim = nd.dot(self.dataset, face)
+        scores = {}
+        for known_id, index in self.names.items():
+            scores[known_id] = max(sim[index]).asnumpy()
 
-            if max(scores.values()) > self.args.threshold_face:
-                faces_names[max(scores, key=scores.get)] = box
-            else:
-                unknown_faces.append(box)
+        if max(scores.values()) > self.args.threshold_face:
+            return max(scores, key=scores.get)
+        else:
+            return None
 
-        if len(unknown_faces):
-            faces_names['unknown'] = unknown_faces
+    def name_person(self, frame, points, box, person=None):
+        # This function will verify if a person face is part or not of the dataset
+        # Depending if the pose of the face is correct or if the face is in the dataset a name will be assigned
+        if person is None:  # Create a new object
+            person = Person(points[2], box, args.threshold_l2)
+        else:  # Update variables
+            person.pre_point = points[2]
+            person.box = box
 
-        return faces_names
+        if person.known is not None:  # Check if the person has already been checked
+            return
+        # Obtain face orientation and if looking to the front take a screenshot
+        ret, l, r, u, d = self.detector.check_large_pose(points, box[1:])
+        if not ret and l < self.pv[0] and r < self.pv[1] and u < self.pv[2] and d < self.pv[3]:
+            points = np.array([np.concatenate([points[:, 0], points[:, 1]])])
+            cropped_face = self.model.detector.extract_image_chips(frame, points, self.crop_resolution, 0.37)[0]
+            name = self.name_face(cropped_face)
+            if name is not None:  # Face verified on the dataset
+                person.name = name
+                person.face = cropped_face
+                person.known = True
+            else:  # Face is not in dataset
+                person.known = False
+
+        if person not in self.persons:
+            self.persons.append(person)
+
+    def identify(self, frame, bbox, points):
+        # Identify the person based on face verification or coordinate approximation
+
+        if not self.persons and bbox.shape[0]:  # Check if persons list is empty and there are detected faces
+            for i in range(bbox.shape[0]):
+                box = bbox[i].astype(np.int)
+                landmark5 = points[i].astype(np.int)
+                self.name_person(frame, landmark5, box)
+            landmark5 = points[-1].astype(np.int) + 20
+            self.name_person(frame, landmark5, box)
+
+        elif len(self.persons) == bbox.shape[0]:  # Find corresponding person for each coordinate
+            for i in range(bbox.shape[0]):
+                box = bbox[i].astype(np.int)
+                landmark5 = points[i].astype(np.int)
+                distances = np.array([p.l2_distance(landmark5[2]) for p in self.persons])
+                person_index = np.where(distances > 0, distances, np.inf).argmin()
+                self.name_person(frame, landmark5, box, person=self.persons[person_index])
+
+        elif len(self.persons) < bbox.shape[0]:  # Identify previous persons and add new ones
+            boxes = [box for box in bbox.astype(np.int)]
+            landmarks = [point for point in points.astype(np.int)]
+
+            centers = [p[2] for p in landmarks]
+            distances = np.array([p.l2_distance(center) for p in self.persons for center in centers]).reshape(
+                len(self.persons), -1)
+            known_index = np.where(distances > 0, distances, np.inf).argmin(axis=1)
+            unknown_index = np.nonzero(np.where(distances == -1, distances, 0)[0])[0]
+
+            # update known persons
+            for ki, p in zip(known_index, range(len(self.persons))):
+                self.name_person(frame, landmarks[ki], boxes[ki], person=self.persons[p])
+            # add unknown persons
+            for uk in unknown_index:
+                self.name_person(frame, landmarks[uk], boxes[uk])
+
+        elif len(self.persons) > bbox.shape[0]:
+            if not bbox.shape[0]:  # There is no faces detected
+                del self.persons[:]  # Empty the hole list
+                return
+            # Identify previous persons and remove the ones that disappeared
+            boxes = [box for box in bbox.astype(np.int)]
+            landmarks = [point for point in points.astype(np.int)]
+
+            centers = [p[2] for p in landmarks]
+            distances = np.array([p.l2_distance(center) for p in self.persons for center in centers]).reshape(
+                len(self.persons), -1)
+            known_index = np.where(distances > 0, distances, np.inf).argmin(axis=1)
+            d_index = []
+            for _ in range(len(self.persons)-bbox.shape[0]):
+                d = np.where(distances == np.max(distances))[0][0]
+                distances = np.delete(distances, d, axis=0)
+                d_index.append(d)
+            known_index = np.delete(known_index, d_index)
+
+            # update known persons
+            for ki, p in zip(known_index, range(len(self.persons))):
+                self.name_person(frame, landmarks[ki], boxes[ki], person=self.persons[p])
+            # delete disappeared persons from list
+            for d in d_index:
+                del self.persons[d]
 
     def detect_faces(self, frame):
-        resolution = int(self.args.image_size.split(',')[0])
+        if self.dataset is None:
+            self.load_features()
         # run detector
         results = self.detector.detect(frame, threshold=args.threshold)
         if results is not None:
-            total_boxes = results[0]
-            points = results[1]
-            if self.args.pose:
-                return self.draw_pose(frame, points, total_boxes)
-
-            else:
-                faces_names = {'unknown': [box for box in total_boxes]}
-                return self.draw_names(frame, faces_names)
-
-        else:
-            return frame
+            return results
 
 
 if __name__ == '__main__':
